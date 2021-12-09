732cfc16e68d94a363b6c23501995578a03317ad
doretienshinh <doretienshinh2@gmail.com>
2021-12-09 12:56:45 +0000

    add description 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_09_120919) do
+ActiveRecord::Schema.define(version: 2021_12_09_125154) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_12_09_120919) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description2"
   end
 
   create_table "users", force: :cascade do |t|
