05e0bfb636d28756e2233abeb6a6730c03731b5d
diemntn-184068 <ngocdiem5102000@gmail.com>
2021-12-13 08:30:44 +0000

    change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_10_095339) do
+ActiveRecord::Schema.define(version: 2021_12_12_141523) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2021_12_10_095339) do
     t.integer "user_id"
     t.integer "restaurant_id"
     t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "restaurant_food_reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_food_id"
+    t.text "review"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
